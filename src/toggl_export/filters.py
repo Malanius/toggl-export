a7ab4babc4b0491f6bf7f89ee4a36e2ad96777b6
Michal Slota <malaniusprivierre@gmail.com>
2025-06-30 09:44:07 +0200

    feat(export): add entry date to validation error message

--- src/toggl_export/filters.py
+++ src/toggl_export/filters.py
@@ -1,4 +1,5 @@
 from rich import print as rprint
+from datetime import datetime
 
 from toggl_export.models.time_entry import TimeEntry
 
@@ -10,8 +11,9 @@ def filter_by_workspace(entries: list[TimeEntry], workspace_id: str) -> list[Tim
 def filter_by_client(entries: list[TimeEntry], client_name: str) -> list[TimeEntry]:
     for entry in entries:
         if not entry.get("client_name"):
+            entry_date = datetime.fromisoformat(entry["start"]).strftime("%Y-%m-%d")
             rprint(
-                f"[red] Entry \"{entry['description']}\" does not have project or client information!"
+                f"[red] Entry \"{entry['description']}\" from {entry_date} does not have project or client information!"
             )
             exit(1)
     return [entry for entry in entries if entry["client_name"] == client_name]
